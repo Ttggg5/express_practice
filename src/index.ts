257a205f2de46ec2f2efb4474548753f57e2751e
知道 不 <s95510691@gmail.com>
2025-11-20 16:08:17 +0800

    channge origin setting

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ socketIoServer.startServer();
 
 // Middleware
 app.use(cors({
-  origin: true, // frontend URL
+  origin: '*', // frontend URL
   credentials: true // allow cookies from frontend
 }));
 app.use(express.json());
